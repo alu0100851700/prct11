da6660dcdd4e13f8fab85455c0b50dcd277da244
ASP <alu0100769609@ull.edu.es>
2015-11-03 18:01:09 +0000

    Añadidas pruebas iniciales (Insertar datos del libro)

--- spec/biblio_gem_spec.rb
+++ spec/biblio_gem_spec.rb
@@ -1,11 +1,30 @@
 require 'spec_helper'
+require 'biblio_gem.rb'
 
 describe BiblioGem do
+  def setup
+      @bib1 = BiblioGem.new("Autor1","Titulo1","Fecha1","ISBN1")
+  end
+
   it 'has a version number' do
     expect(BiblioGem::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
+   describe "Existe un libro" do
+      it "El nombre del autor es correcto" do
+         @bib1.autor.should eq("Autor1")
+      end
+
+      it "El título del libro es correcto" do
+         @bib1.titulo.should eq("Titulo1")
+      end
+
+      it "La fecha del libro es correcta" do
+         @bib1.fecha.should eq("Fecha1")
+      end
+
+      it "ISBN correcto" do
+         @bib1.isbn.should eq("ISBN1")
+      end
+   end
 end
