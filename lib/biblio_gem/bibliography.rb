e0f620a2ed52393709a8cb3f84e617d3b099eab3
David Redondo Durand <alu0100851700@ull.edu.es>
2015-11-03 21:17:02 +0000

    Añadido correctamente metodo to_s

--- lib/biblio_gem/bibliography.rb
+++ lib/biblio_gem/bibliography.rb
@@ -6,5 +6,13 @@ module BiblioGem
 			@autor, @titulo, @fecha, @isbn = autor, titulo, fecha, isbn
 		end
 
+		def to_s
+			if @autor.is_a? Array
+				"Autor/es: #{@autor.join(", ")}\n Titulo: #{@titulo}\n Fecha: #{@fecha}\n ISBN: #{@isbn}\n"
+			else
+				"Autor/es: #{@autor}\n Titulo: #{@titulo}\n Fecha: #{@fecha}\n ISBN: #{@isbn}\n"
+			end
+		end
+		
 	end
 end
