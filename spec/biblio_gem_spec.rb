2b871ee6212fbeca9b2891c5aedcd91f2b9ed08f
ASP <alu0100769609@ull.edu.es>
2015-11-03 20:38:45 +0000

    Arreglados mensajes de tests y añadido test de impresión de datos mediante to_s

--- spec/biblio_gem_spec.rb
+++ spec/biblio_gem_spec.rb
@@ -3,13 +3,12 @@ require 'spec_helper'
 require 'biblio_gem'
 
 describe BiblioGem do
- 
-  it 'has a version number' do
-    expect(BiblioGem::VERSION).not_to be nil
-  end
+    
+    it 'has a version number' do
+        expect(BiblioGem::VERSION).not_to be nil
+    end
 
-   describe "Existe un libro" do
-
+   describe "Almacenamiento de datos (1 autor):" do
       before :each do
         @bib1 = BiblioGem::Bibliography.new("Autor1","Titulo1","Fecha1","ISBN1")
       end
@@ -30,4 +29,42 @@ describe BiblioGem do
          @bib1.isbn.should eq("ISBN1")
       end
    end
+   
+   describe "Almacenamiento de datos (2 autores):" do
+      before :each do
+         @bib2 = BiblioGem::Bibliography.new(["Autor1","Autor2"],"Titulo2","Fecha2","ISBN2")
+      end
+      
+      it "El nombre de ambos autores son correctos" do
+         @bib2.autor.should eq(["Autor1","Autor2"])
+      end
+      
+      it "El título del libro es correcto" do
+         @bib2.titulo.should eq("Titulo2")
+      end
+
+      it "La fecha del libro es correcta" do
+         @bib2.fecha.should eq("Fecha2")
+      end
+
+      it "ISBN correcto" do
+         @bib2.isbn.should eq("ISBN2")
+      end
+   end
+   
+   describe "Imprimiendo datos" do
+      before :each do
+         @bib1 = BiblioGem::Bibliography.new("Autor1","Titulo1","Fecha1","ISBN1")
+         @bib2 = BiblioGem::Bibliography.new(["Autor1","Autor2"],"Titulo2","Fecha2","ISBN2")
+      end
+       
+      it "Imprimiendo datos 1" do
+         @bib1.to_s.should eq("Autor/es: Autor1\n Titulo: Titulo1\n Fecha: Fecha1\n ISBN: ISBN1\n")
+      end
+      
+      it "Imprimiendo datos 2" do
+         @bib2.to_s.should eq("Autor/es: Autor1, Autor2\n Titulo: Titulo1\n Fecha: Fecha1\n ISBN: ISBN1\n")
+      end
+   end
+   
 end
