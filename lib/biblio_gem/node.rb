421ffe675acbccd6c4f46e6ccf6073ebf1b66852
alu0100851700 <alu0100851700@ull.edu.es>
2015-11-10 17:43:04 +0000

    Creado codigo de node, dando problemas por las expectativas

--- lib/biblio_gem/node.rb
+++ lib/biblio_gem/node.rb
@@ -1,7 +1,15 @@
 module BiblioGem
     Node = Struct.new(:value, :next) do
         
+        attr_accessor :value, :next
+        
         def initialize(args)
+            @value = args[:value]
+            @next = args[:next]
+        end
+        
+        def to_s
+            @value.to_s
         end
         
     end
